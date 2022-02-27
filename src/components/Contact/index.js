2e81f844db785b550897b461d4eb6ed064ee9f83
Brandon Hoskins <branjames117@gmail.com>
2022-02-26 18:43:29 -0600

    added contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,78 @@
+import { useState } from 'react';
+
 export default function Contact() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [validName, setValidName] = useState(true);
+  const [validEmail, setValidEmail] = useState(true);
+  const [validMessage, setValidMessage] = useState(true);
+  const [messageSent, setMessageSent] = useState(false);
+
+  const { name, email, message } = formState;
+
+  function validateEmail(email) {
+    var re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  function handleChange(e) {
+    if (e.target.name === 'name') {
+      if (e.target.value.trim() === '') {
+        setValidName(false);
+      } else {
+        setValidName(true);
+      }
+    }
+
+    if (e.target.name === 'email') {
+      let isValid = validateEmail(e.target.value);
+      if (e.target.value.trim() === '') {
+        setValidEmail(false);
+      } else {
+        setValidEmail(true);
+      }
+
+      if (!isValid) {
+        setValidEmail(false);
+      } else {
+        setValidEmail(true);
+      }
+    }
+
+    if (e.target.name === 'message') {
+      if (e.target.value.trim() === '') {
+        setValidMessage(false);
+      } else {
+        setValidMessage(true);
+      }
+    }
+
+    if (!validName || !validEmail || !validMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      !formState.name ||
+      !formState.email ||
+      !formState.message ||
+      !validName ||
+      !validEmail ||
+      !validMessage
+    ) {
+      return;
+    } else {
+      setMessageSent(true);
+    }
+  }
+
   return (
     <section id='contact'>
       <div className='container'>
@@ -42,6 +116,54 @@ export default function Contact() {
             </li>
           </ul>
         </article>
+        <form id='contact-form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='name'>Name</label>
+            <input
+              autoComplete='off'
+              type='text'
+              name='name'
+              defaultValue={name}
+              onBlur={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email</label>
+            <input
+              autoComplete='off'
+              type='email'
+              name='email'
+              defaultValue={email}
+              onBlur={handleChange}
+            />
+          </div>
+          <div>
+            <textarea
+              autoComplete='off'
+              name='message'
+              rows='5'
+              defaultValue={message}
+              placeholder='What can we accomplish together?'
+              onBlur={handleChange}
+            />
+          </div>
+          {true && (
+            <div>
+              <p className='error-text'>
+                {!validName && <span>Your name is required.</span>}
+                {!validEmail && <span>A valid email is required.</span>}
+                {!validMessage && <span>A message is required.</span>}
+              </p>
+            </div>
+          )}
+          {!messageSent ? (
+            <button type='submit'>Send Message</button>
+          ) : (
+            <button type='submit' disabled>
+              Thank you for your message!
+            </button>
+          )}
+        </form>
       </div>
     </section>
   );
