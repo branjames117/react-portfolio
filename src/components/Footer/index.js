dd292c749e164fb011d590acdd0ce3ee8ccf7312
Brandon Hoskins <branjames117@gmail.com>
2022-02-27 00:33:46 -0600

    added social icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,17 @@
 export default function Footer() {
   return (
     <footer>
+      <p id='footer-icons'>
+        <a href='https://github.com/branjames117'>
+          <i className='fab fa-github-square'></i>
+        </a>
+        <a href='https://www.facebook.com/branjames117'>
+          <i className='fab fa-facebook-square'></i>
+        </a>
+        <a href='https://www.linkedin.com/in/branjames117/'>
+          <i className='fab fa-linkedin'></i>
+        </a>
+      </p>
       <p>
         <a href='mailto:branjames117@gmail.com'>bran james</a> &copy;
         twozerotwotwo
